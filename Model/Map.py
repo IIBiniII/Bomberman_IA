878e8eff051ab67242228346559bf1ed9886b365
E226752U <floran.martel@etu.univ-nantes.fr>
2023-10-17 18:56:11 +0200

    map

---
+++ Model/Map.py
@@ -0,0 +1,53 @@
+from random import randint
+
+class Map:
+
+    def __init__(self) -> None:
+        
+        self.Carte = [
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+                     [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+                     [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+                     [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+                     [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+                     [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+                     [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+                     [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+                     [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+                     [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+                     [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+                    ]
+    
+    def placerCaise(self):
+        for nul in range(len(self.Carte)):
+            for nuc in range(len(self.Carte[nul])):
+                if self.Carte[nul][nuc] == 1 and randint(0,100) <= 60:
+                    if nul not in (1,2,10,11) or nuc not in (1,2,12,13):
+                        self.Carte[nul][nuc] = 2
+                    
+
+
+
+    def affichier(self):
+        
+        text = ""
+
+        for l in self.Carte:
+            for v in l:
+                if v == 0:
+                    text+="▓▓▓"
+                elif v == 1:
+                    text+="░░░"
+                elif v == 2:
+                    text+="▒▒▒"
+
+            text+="\n"
+        
+        print(text)
+
+
+m = Map()
+m.placerCaise()
+m.affichier()
